adaa6cf49a2a2ddde96347f89a39145718bc981f
Leon Zhang <lwzhang@andrew.cmu.edu>
2014-02-01 00:12:29 -0500

    clean up all these things and start over

--- NoteRight/src/com/noteright/HomeActivity.java
+++ NoteRight/src/com/noteright/HomeActivity.java
@@ -1,21 +1,21 @@
-package com.example.noteright;
+package com.noteright;
 
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
-public class MainActivity extends Activity {
+public class HomeActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.activity_home);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
+		getMenuInflater().inflate(R.menu.home, menu);
 		return true;
 	}
 
