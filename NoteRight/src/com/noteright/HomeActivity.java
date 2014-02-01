1d170cad6f7b274901a22e761f9a5db272c18726
Leon Zhang <lwzhang@andrew.cmu.edu>
2014-02-01 02:08:28 -0500

    here's an image. im bad at android ugh w/e

--- NoteRight/src/com/noteright/HomeActivity.java
+++ NoteRight/src/com/noteright/HomeActivity.java
@@ -2,7 +2,9 @@ package com.noteright;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
 
 public class HomeActivity extends Activity {
 
@@ -17,6 +19,10 @@ public class HomeActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.home, menu);
 		return true;
+	}
+	
+	public void toGalleryPage(View view){
+		// TODO: wtf i dunno
 	}
 
 }
